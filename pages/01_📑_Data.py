01c5b66dad07fe08b0f0f69142022ddc45745f00
Bambam <aminuoluwarotimi@gmail.com>
2024-06-01 10:39:28 -0700

    Home page and venv

---
+++ pages/01_📑_Data.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pyodbc
+
+st.set_page_config(
+    page_title = 'Home Page',
+    page_icon = '💒',
+    layout = 'wide'
+)
+
+st.title("Telco Customer Churn Prediction Database 🔋")
+
+# Create a connection to database
+# Query database
+
+def init_connection():
+    pyodbc.connect(
+        "driver='{SQL SERVER}' SERVER = ",
+        + st.secrets['server']
+        + ";database"
+        + st.secrets['database']
+        + ";uid="
+        + st.secrets['username']
+        + ";pwd="
+        + st.secrets['password']
+    )
