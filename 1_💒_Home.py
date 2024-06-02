fde0e9f31e4c8f86945e59831af7555e0d08540a
Bambam <aminuoluwarotimi@gmail.com>
2024-06-02 13:44:53 -0700

    homepage

--- 1_💒_Home.py
+++ 1_💒_Home.py
@@ -11,8 +11,29 @@ st.title("Welcome to :violet[Telco Customer Churn Prediction App]")
 
 
 def app():
-    choice = st.selectbox('Login/Sign Up', ['Login', 'Sign Up'])
+    def login():
+        try:
+            display_name = auth.get_user_by_email(email)
+            st.write('Login Successfully')
 
+        except:
+            st.warning('Invalid email or password')
+   
+
+    col1, col2 = st.columns(2)
+    with col2:
+        # Your additional content goes here
+        st.write("Column 1 content")
+
+    with col1:
+        st.write('## How to run application')
+        st.code('''
+        # activate virtual environment
+        env/scripts/activate
+        streamlit run home.py
+        ''')
+        st.link_button('Repository on Github', url='https://github.com/Koanim/LP4-Telco-Customer-Churn-Prediction-APP/blob/dev/1_%F0%9F%92%92_Home.py')
+        choice = st.selectbox('Login/Sign Up', ['Login', 'Sign Up'], key='unique_selectbox_key')
     if choice == 'Login':
         with st.form("login_form"):
             username = st.text_input('Username')
