92c2da5667f1e8dca3f509fbecb334e90a0b132d
Bambam <aminuoluwarotimi@gmail.com>
2024-06-02 01:18:52 -0700

    Signup/login page

--- 1_💒_Home.py
+++ 1_💒_Home.py
@@ -1,41 +1,36 @@
 import streamlit as st
 
+
 st.set_page_config(
     page_title = 'Home Page',
     page_icon = '💒',
     layout = 'wide'
 )
 
-st.title("Telco Customer Churn Prediction App")
-name = st.text_input("Customer Name")  
-st.write(f'Hello {name}!')
-
-Gender = st.selectbox("Select your Gender:", ["Male", "Female"])
-SeniorCitizen = st.selectbox("Select SeniorCitizen:", ["Yes", "No"])
-Partner = st.selectbox("Select Partner", ["Yes", "No"])
-Dependents = st.selectbox("Select Dependents", ["Yes", "No"])
-Tenure = st.number_input("Enter Tenure", key="Tenure_input")
-PhoneService = st.selectbox("Select PhoneService", ["Yes", "No"])
-MultipleLines = st.selectbox("Select MultipleLines", ["Yes", "No"])
-InternetService = st.selectbox("Select InternetService", ['DSL', 'Fiber optic', 'No'])
-OnlineSecurity= st.selectbox("Select OnlineSecurity", ["Yes", "No"])
-OnlineBackup = st.selectbox("Select OnlineBackup", ["Yes", "No"])
-DeviceProtection =st.selectbox("Select DeviceProtection", ["Yes", "No"])
-TechSupport = st.selectbox("Select TechSupport", ["Yes", "No"])
-StreamingTV = st.selectbox("Select StreamingTV", ["Yes", "No"])
-StreamingMovies = st.selectbox("Select StreamingMovies", ["Yes", "No"])
-Contract = st.selectbox("Select Contract", ['Month-to-month', 'One year', 'Two year'])
-PaperlessBilling = st.selectbox("Select PaperlessBilling", ["Yes", "No"])
-PaymentMethod= st.selectbox("Select PaymentMethod", ['Bank transfer (automatic)', 'Credit card (automatic)', 'Electronic check', 'Mailed check'])
-MonthlyCharges= st.number_input("Enter MonthlyCharges", key="MonthlyCharges_input")
-
-# Create a function to calculate total charges
-def calculate_total_charges(tenure, monthly_charges):
-    total_charges = tenure * monthly_charges
-    return total_charges
-
-TotalCharges = st.write("Total Charges", calculate_total_charges(Tenure, MonthlyCharges))
+st.title("Welcome to :violet[Telco Customer Churn Prediction App]")
 
 
-if st.button("Predict"):
-    st.text("The Customer churn rate is classified to be : '{''}'.")
+def app():
+    choice = st.selectbox('Login/Sign Up', ['Login', 'Sign Up'])
+
+    if choice == 'Login':
+        with st.form("login_form"):
+            username = st.text_input('Username')
+            password = st.text_input('Password', type='password')
+            login_button = st.form_submit_button('Login')
+
+        if login_button:
+            st.success(f'Logged in as {username}')
+    
+    else:
+        with st.form("signup_form"):
+            username = st.text_input('Username')
+            password = st.text_input('Password', type='password')
+            email_address = st.text_input('Email Address')
+            signup_button = st.form_submit_button('Sign up')
+
+        if signup_button:
+            st.success(f'Signed up as {username}')
+
+if __name__ == '__main__':
+    app()
---
+++ pages/03_❔_Predict.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = 'Predict Page',
+    page_icon = '💒',
+    layout = 'wide'
+)
+
+
+st.title("Telco Customer Churn Prediction App")
+name = st.text_input("Customer Name")  
+st.write(f'Hello {name}!')
+
+Gender = st.selectbox("Select your Gender:", ["Male", "Female"])
+SeniorCitizen = st.selectbox("Select SeniorCitizen:", ["Yes", "No"])
+Partner = st.selectbox("Select Partner", ["Yes", "No"])
+Dependents = st.selectbox("Select Dependents", ["Yes", "No"])
+Tenure = st.number_input("Enter Tenure", key="Tenure_input")
+PhoneService = st.selectbox("Select PhoneService", ["Yes", "No"])
+MultipleLines = st.selectbox("Select MultipleLines", ["Yes", "No"])
+InternetService = st.selectbox("Select InternetService", ['DSL', 'Fiber optic', 'No'])
+OnlineSecurity= st.selectbox("Select OnlineSecurity", ["Yes", "No"])
+OnlineBackup = st.selectbox("Select OnlineBackup", ["Yes", "No"])
+DeviceProtection =st.selectbox("Select DeviceProtection", ["Yes", "No"])
+TechSupport = st.selectbox("Select TechSupport", ["Yes", "No"])
+StreamingTV = st.selectbox("Select StreamingTV", ["Yes", "No"])
+StreamingMovies = st.selectbox("Select StreamingMovies", ["Yes", "No"])
+Contract = st.selectbox("Select Contract", ['Month-to-month', 'One year', 'Two year'])
+PaperlessBilling = st.selectbox("Select PaperlessBilling", ["Yes", "No"])
+PaymentMethod= st.selectbox("Select PaymentMethod", ['Bank transfer (automatic)', 'Credit card (automatic)', 'Electronic check', 'Mailed check'])
+MonthlyCharges= st.number_input("Enter MonthlyCharges", key="MonthlyCharges_input")
+
+# Create a function to calculate total charges
+def calculate_total_charges(tenure, monthly_charges):
+    total_charges = tenure * monthly_charges
+    return total_charges
+
+TotalCharges = st.write("Total Charges", calculate_total_charges(Tenure, MonthlyCharges))
+
+
+if st.button("Predict"):
+    st.text("The Customer churn rate is classified to be : '{''}'.")
