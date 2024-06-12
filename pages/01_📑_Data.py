2713c4ad60dd420d6ba8525c092209cca4349246
Victor Nyarko Anim <victor.nyarko@ymail.com>
2024-06-12 15:14:58 +0000

    dashboard

--- pages/01_📑_Data.py
+++ pages/01_📑_Data.py
@@ -10,12 +10,6 @@ st.set_page_config(
 )
 
 
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-local_css("style.css")
-
 
 st.title('Telco Customer Churn Data 🗃')
 
--- pages/02_📈_Dashboard.py
+++ pages/02_📈_Dashboard.py
@@ -13,11 +13,6 @@ st.set_page_config(
     layout = 'wide'
 )
 
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-local_css("style.css")
 
 st.title('EDA & Dashboard')
 
@@ -27,32 +22,59 @@ def load_concat_data():
     return concat_df
 
 concat_df = load_concat_data()
-st.write(concat_df)
 
+if st.checkbox('Show data'):
+    st.subheader('Telco churn data')
+    st.write(concat_df)
 
-fig = px.histogram(concat_df, x='tenure', nbins=100, title='Distribution of Tenure')
+col1, col2, col3 = st.columns(3)
 
-st.plotly_chart(fig)
+with col1:
+    #st.subheader('Number of Churned Customers')
+    #fig = px.histogram(concat_df, x='Churn', color='Churn', color_discrete_map={'Yes':'red', 'No':'blue'}, title='Number of Churned Customers')
+    fig = px.histogram(concat_df, x='tenure', nbins=100, title='Distribution of Tenure')
+    st.plotly_chart(fig)
+with col2:    
+    fig1 = px.histogram(concat_df, x='MonthlyCharges', nbins=100, title='Distribution of Monthly Charges')
+    st.plotly_chart(fig1)   
+    
+    
 
+with col3:
+    
+    fig2 = px.histogram(concat_df, x='TotalCharges', nbins=100, title='Distribution of Total Charges')
+    st.plotly_chart(fig2)
+    
+    sns.set(style="ticks")
+    pairplot = sns.pairplot(concat_df, hue='Churn', height=1.2, aspect=2)
+    plt.subplots_adjust(top=0.9)
+    plt.suptitle('Customer Churn Pairplot', fontsize=20)
 
-sns.set(style="ticks")
-pairplot = sns.pairplot(concat_df, hue='Churn', height=1.2, aspect=2)
+    for ax in pairplot.axes.flatten():
+        for label in ax.get_xticklabels():
+            label.set_rotation(25)
+        for label in ax.get_yticklabels():
+            label.set_rotation(25)
+    st.pyplot(pairplot)
+    
+col4, col5 = st.columns(2)
+with col4:
+    fig4 = px.histogram(concat_df, x='Churn', color='Churn', color_discrete_map={'Yes':'red', 'No':'blue'}, title='Number of Churned Customers')
+    st.plotly_chart(fig4)
+    
+    fig5 = px.histogram(concat_df, x='Contract', color='Churn', color_discrete_map={'Yes':'red', 'No':'blue'}, title='Number of Churned Customers')
+    st.plotly_chart(fig5)
+    
+    fig6 = px.histogram(concat_df, x='PaymentMethod', color='Churn', color_discrete_map={'Yes':'red', 'No':'blue'}, title='Number of Churned Customers')
+    st.plotly_chart(fig5)
 
-for ax in pairplot.axes.flatten():
-    for label in ax.get_xticklabels():
-        label.set_rotation(25)
-    for label in ax.get_yticklabels():
-        label.set_rotation(25)
+with col5:    
+    catplot1 = sns.catplot(concat_df, x='gender', y= 'tenure', hue='Churn', kind='violin', palette=['blue', 'red'], height=3, aspect=2 )
+    st.subheader('How long Female and Male stay with Telco before Churning')
+    plt.show()
+    st.pyplot(catplot1)
 
-# Display the plot in Streamlit
-st.pyplot(pairplot)
-
-catplot1 = sns.catplot(concat_df, x='gender', y= 'tenure', hue='Churn', kind='violin', palette=['blue', 'red'], height=3, aspect=2 )
-st.subheader('How long Female and Male stay with Telco before Churning')
-plt.show()
-st.pyplot(catplot1)
-
-catplot2 = sns.catplot(concat_df, x='Contract', y= 'tenure', hue='Churn', kind='box', aspect=1, palette=['blue', 'red'])
-plt.title('How long does it take each contract type before Churning')
-plt.show()
-st.pyplot(catplot2)
+    catplot2 = sns.catplot(concat_df, x='Contract', y= 'tenure', hue='Churn', kind='box', aspect=1, palette=['blue', 'red'])
+    plt.title('How long does it take each contract type before Churning')
+    plt.show()
+    st.pyplot(catplot2)
